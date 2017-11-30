ff0e82c8fc37b93b36266ad37b1876700eb74813
Thomas Gull <tg@angela-bruderer.local>
2017-11-30 14:58:30 +0100

    Creating template for events

---
+++ events/events.php
@@ -0,0 +1,60 @@
+<!-- Event no1 -->
+<section class="events bgPrimary">
+	<div class="grid-container grid-x">
+		<div class="small-12 cell">
+			<div class="grid-x align-center">
+				<div class="small-10 medium-6 large-6 cell">
+					<h3 class="text-center">
+						<?php if ($flag1 !== "") {echo "<img class=\"flags\" src=\"../img/flags/$flag1.png\">";} ?>
+						<?php echo $titel1 ?>
+					</h3>				
+			<h4><?php echo $time1 ?></h4>
+			<p><?php if ($image1 !== "") {echo "<img class=\"eventImage\" src=\"../img/events/$image1\">";} ?><?php echo $description1 ?></p>
+			<h4>Anmeldung</h4>
+			<p>Unser kleiner aber feiner Weinladen bietet Platz für 20 Personen. Bitte senden Sie Ihre Anmeldung bis zum <?php echo $registrationDate1 ?> an <a href="mailto:info@cavatappo.ch">info@cavatappo.ch</a>. Für die Verköstigung verrechnen wir einen kleinen Beitrag von 25 Franken pro Person.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Event no2 -->
+<section class="events bgPrimary">
+	<div class="grid-container grid-x">
+		<div class="small-12 cell">
+			<div class="grid-x align-center">
+				<div class="small-10 medium-6 large-6 cell">
+					<h3 class="text-center">
+						<?php if ($flag2 !== "") {echo "<img class=\"flags\" src=\"../img/flags/$flag2.png\">";} ?>
+						<?php echo $titel2 ?>
+					</h3>				
+			<h4><?php echo $time2 ?></h4>
+			<p><?php if ($image2 !== "") {echo "<img class=\"eventImage\" src=\"../img/events/$image2\">";} ?><?php echo $description2 ?></p>
+			<h4>Anmeldung</h4>
+			<p>Unser kleiner aber feiner Weinladen bietet Platz für 20 Personen. Bitte senden Sie Ihre Anmeldung bis zum <?php echo $registrationDate2 ?> an <a href="mailto:info@cavatappo.ch">info@cavatappo.ch</a>. Für die Verköstigung verrechnen wir einen kleinen Beitrag von 25 Franken pro Person.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Event no3 -->
+<section class="events bgPrimary">
+	<div class="grid-container grid-x">
+		<div class="small-12 cell">
+			<div class="grid-x align-center">
+				<div class="small-10 medium-6 large-6 cell">
+					<h3 class="text-center">
+						<?php if ($flag3 !== "") {echo "<img class=\"flags\" src=\"../img/flags/$flag3.png\">";} ?>
+						<?php echo $titel3 ?>
+					</h3>				
+			<h4><?php echo $time3 ?></h4>
+			<p><?php if ($image3 !== "") {echo "<img class=\"eventImage\" src=\"../img/events/$image3\">";} ?><?php echo $description3 ?></p>
+			<h4>Anmeldung</h4>
+			<p>Unser kleiner aber feiner Weinladen bietet Platz für 20 Personen. Bitte senden Sie Ihre Anmeldung bis zum <?php echo $registrationDate3 ?> an <a href="mailto:info@cavatappo.ch">info@cavatappo.ch</a>. Für die Verköstigung verrechnen wir einen kleinen Beitrag von 25 Franken pro Person.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
---
+++ footer.php
@@ -0,0 +1,50 @@
+<!-- <div class="grid-x grid-pading-x"> -->
+	<footer class="grid-frame">
+		<div class="grid-container grid-x grid-padding-x">
+			<div class="small-12 cell">
+				<div class="grid-x">
+					<div class="small-12 medium-3 large-3 cell align-bottom">
+						<div id="address">
+							<address>
+								<span class="title" itemprop="name">Cavatappo</span><br />
+								<span itemprop="address" itemscope itemtype="https://schema.org/PostalAddress">
+									<span itemprop="streetAddress">Tösstalstrasse 59</span><br />
+									<span itemprop="postalcode">8400</span>
+									<span itemprop="addressLocality">Winterthur</span>
+								</span><br />
+								<span itemprop="telephone">058 455 40 80</span><br />
+								<a href="mailto:info@cavatappo.ch?subject=Kontaktanfrage">
+									<span itemprop="email">info@cavatappo.ch</span>
+								</a>
+							</address>
+						</div>
+					</div>
+					<div class="small-12 medium-3 large-4 cell">
+					</div>
+					<div class="small-12 medium-3 large-3 cell">
+						<!-- <span class="wineClaim">Weine von Angela Bruderer</span>
+						<img src="img/ab_logo_weiss_400.png" alt="Logo Angela Bruderer"> -->
+					</div>
+					<div class="small-6 medium-3 large-2 cell align-bottom">
+						<span class="wineClaim">Weine aus dem Sortiment von Angela Bruderer</span>
+						<a href="https://www.angela-bruderer.ch/weine" target="_blank">
+							<img class="logoFooter" src="img/ab_logo_weiss_400.png" alt="Weine von Angela Bruderer">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- <div class="grid-x">
+			<div class="large-12 cell">
+				<span id="copyright">©<?php echo date('Y') ?>, Cava Tappo AG alle Rechte vorbehalten.</span>
+			</div>
+		</div> -->
+	</footer>
+<!-- </div> -->
+<script src="js/vendor/jquery.js"></script>
+<script src="js/vendor/what-input.js"></script>
+<script src="js/vendor/foundation.js"></script>
+<script src="js/app.js"></script>
+<script>
+	$(document).foundation();
+</script>
